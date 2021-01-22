e07969135ce846a0e76ee0292c023aed90214c83
hellobrian <brianhan@hey.com>
2021-01-22 16:39:08 -0600

    remove titlechanger

--- frontend/components/Navbar/Navbar.js
+++ frontend/components/Navbar/Navbar.js
@@ -1,89 +1,21 @@
-import { useState, useEffect } from "react";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import { IconMenu, IconClose } from "@/components/Icons/Icons";
 import styles from "./Navbar.module.css";
 
-function getRandomInt(max = 3) {
-  return Math.floor(Math.random() * Math.floor(max));
-}
-
-function useRandomInt(max = 3) {
-  const [randomInt, setRandomInt] = useState(max);
-
-  useEffect(() => {
-    setRandomInt(getRandomInt(max));
-  }, [max]);
-
-  return { randomInt };
-}
-
-function useTitleChanger(pathname) {
-  const [title, setTitle] = useState("futurebrian");
-  const { randomInt } = useRandomInt();
-
-  useEffect(() => {
-    switch (pathname) {
-      case "/keycaps":
-        if (randomInt === 0) {
-          setTitle(`keycaps`);
-        }
-
-        if (randomInt === 1) {
-          setTitle(`plastic love`);
-        }
-
-        if (randomInt === 2) {
-          setTitle(`*clackity clack`);
-        }
-
-        break;
-
-      case "/keyboards":
-        if (randomInt === 0) {
-          setTitle(`keyboards`);
-        }
-
-        if (randomInt === 1) {
-          setTitle(`keebs`);
-        }
-
-        if (randomInt === 2) {
-          setTitle(`all the keebs`);
-        }
-        break;
-
-      case "/about":
-        setTitle(`about`);
-        break;
-
-      case "/blog":
-        if (randomInt === 0) {
-          setTitle(`Too long for twitter`);
-        } else {
-          setTitle(`blog`);
-        }
-        break;
-    }
-  }, [pathname, randomInt]);
-
-  return { title };
-}
-
 export function Navbar() {
   const router = useRouter();
-
-  const { title } = useTitleChanger(router.pathname);
 
   return (
     <header className={styles.Navbar}>
       <h1>
         <Link href="/">
           <a className="ff--bebas" title="click here to go home">
-            {title}
+            futurebrian
           </a>
         </Link>
       </h1>
+
       <nav>
         {router.pathname === "/menu" ? (
           <Link href="/">
