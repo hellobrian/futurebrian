48ecbb3d01d7f7b1b14a682152cb385b8c93b788
hellobrian <brianhan@hey.com>
2021-01-22 16:07:02 -0600

    add eslint

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,27 @@
+/* eslint-disable no-undef */
+const config = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+  ],
+
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+  plugins: ["react"],
+  rules: {},
+  globals: {
+    React: "writable",
+  },
+};
+
+module.exports = config;
