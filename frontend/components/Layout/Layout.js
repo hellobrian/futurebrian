c61c080c68d29ff53546880533ac2c65e150acd0
Brian Han <hellobrian@users.noreply.github.com>
2021-01-22 21:57:05 -0600

    Scroll aware navbar (#1)
    * show and hide navbar while scrolling
    
    * add prop types to layout

--- frontend/components/Layout/Layout.js
+++ frontend/components/Layout/Layout.js
@@ -1,8 +1,37 @@
+import { useState, useEffect } from "react";
+import PropTypes from "prop-types";
 import Head from "next/head";
 import { Navbar } from "@/components/Navbar/Navbar";
 import styles from "./Layout.module.css";
+import classnames from "classnames";
 
 export function Layout({ children, ...props }) {
+  const [prevScrollPosition, setPrevScrollPosition] = useState(null);
+
+  useEffect(() => {
+    setPrevScrollPosition(window.pageYOffset);
+  }, []);
+
+  const [isVisible, setVisible] = useState(true);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollPosition = window.pageYOffset;
+      const visibiilty = prevScrollPosition > currentScrollPosition;
+
+      setPrevScrollPosition(currentScrollPosition);
+      setVisible(visibiilty);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [prevScrollPosition]);
+
+  const navbarClass = classnames(styles.NavbarWrapper, {
+    [styles.NavBarHidden]: !isVisible,
+  });
+
   return (
     <>
       <Head>
@@ -11,11 +40,18 @@ export function Layout({ children, ...props }) {
       </Head>
 
       <main {...props} className={styles.Layout}>
-        <div style={{ position: "fixed", width: "100%" }}>
+        <div className={navbarClass}>
           <Navbar />
         </div>
-        <div style={{ gridRowStart: 2 }}>{children}</div>
+        <div style={{ gridRowStart: 2 }}>
+          {/* <button onClick={() => setVisible(!isVisible)}>toggle</button> */}
+          {children}
+        </div>
       </main>
     </>
   );
 }
+
+Layout.propTypes = {
+  children: PropTypes.node.isRequired,
+};
