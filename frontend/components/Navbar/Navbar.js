84de71047e0012a9dc0f4448f6732f04c76aaec6
hellobrian <brianhan@hey.com>
2021-01-25 16:20:13 -0600

    center heading in Navbar

--- frontend/components/Navbar/Navbar.js
+++ frontend/components/Navbar/Navbar.js
@@ -5,43 +5,29 @@ import styles from "./Navbar.module.css";
 
 export function Navbar() {
   const router = useRouter();
+  const goBack = () => router.back();
 
   return (
     <header className={styles.Navbar}>
       <nav>
         {router.pathname === "/menu" ? (
-          <>
-            <button
-              type="button"
-              className={styles.Toggle}
-              onClick={() => router.back()}
-            >
-              <IconClose className={styles.IconClose}></IconClose>
-            </button>
-            <h1>
-              <Link href="/">
-                <a className="ff--bebas" title="click here to go home">
-                  futurebrian
-                </a>
-              </Link>
-            </h1>
-          </>
+          <button type="button" className={styles.Button} onClick={goBack}>
+            <IconClose />
+          </button>
         ) : (
-          <>
-            <Link href="/menu">
-              <a className={styles.Toggle}>
-                <IconMenu className={styles.IconMenu}></IconMenu>
-              </a>
-            </Link>
-            <h1>
-              <Link href="/">
-                <a className="ff--bebas" title="click here to go home">
-                  futurebrian
-                </a>
-              </Link>
-            </h1>
-          </>
+          <Link href="/menu">
+            <a className={styles.Button}>
+              <IconMenu />
+            </a>
+          </Link>
         )}
+        <h1 className={styles.Heading}>
+          <Link href="/">
+            <a className="ff--bebas" title="click here to go home">
+              futurebrian
+            </a>
+          </Link>
+        </h1>
       </nav>
     </header>
   );
