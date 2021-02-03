eec050e8c2c0747a333c12720ddd4fbc863248d1
Brian Han <hellobrian@users.noreply.github.com>
2021-02-02 21:30:16 -0600

    limit MainImage width (#7)
    * limit MainImage width
    
    * Remove borders
    
    * wait for image to load before showing it
    
    * more content shift fixes

--- layouts/DetailPageLayout.js
+++ layouts/DetailPageLayout.js
@@ -1,3 +1,4 @@
+import { useState, useRef, useEffect, useCallback } from "react";
 import ReactMarkdown from "react-markdown";
 import styles from "./DetailPageLayout.module.css";
 
@@ -7,18 +8,90 @@ function Tag({ children }) {
   return <li className={styles.ListItem}>{children}</li>;
 }
 
+function MainImage({ imgUrl, alternativeText }) {
+  const image = useRef();
+  const [loaded, setLoaded] = useState(null);
+  const [opacity, setOpacity] = useState(0);
+
+  const handleLoad = useCallback(() => {
+    if (loaded === null && image.current && image.current.src === imgUrl) {
+      setLoaded(true);
+    }
+  }, [loaded, image, imgUrl]);
+
+  useEffect(() => {
+    if (
+      loaded === null &&
+      image.current &&
+      image.current.src === imgUrl &&
+      image.current.complete
+    ) {
+      setLoaded(true);
+    }
+  }, [loaded, image, imgUrl]);
+
+  useEffect(() => {
+    if (loaded == true) setOpacity(1);
+  }, [loaded]);
+
+  return (
+    <div className={styles.MainImage}>
+      <img
+        ref={image}
+        data-src={imgUrl}
+        src={imgUrl}
+        alt={alternativeText}
+        onLoad={handleLoad}
+        style={{
+          opacity,
+          transition: "opacity 200ms",
+        }}
+      />
+    </div>
+  );
+}
+
+function YouTube() {
+  return (
+    <details className={styles.YouTube}>
+      <summary>Sound Test Video</summary>
+      <div className={styles.EmbedWrapper}>
+        <iframe
+          width="100%"
+          height="auto"
+          src="https://www.youtube.com/embed/g-vBr9a36NE"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+        ></iframe>
+      </div>
+    </details>
+  );
+}
+
+function Gallery() {
+  return (
+    <div className={styles.Gallery}>
+      <img
+        className={styles.Image}
+        src="/keyboards/photos/ava-yellow.jpg"
+        alt="ava"
+      />
+    </div>
+  );
+}
+
 export function DetailPageLayout({ heroImage, name, blog, round }) {
-  console.log({ blog });
   const imgUrl = heroImage ? `${strapiUrl}${heroImage.url}` : null;
   return (
     <div className={styles.Container}>
       {heroImage && (
-        <img
-          className={`${styles.MainImage} ${styles.Full}`}
-          src={imgUrl}
-          alt={heroImage.alternativeText}
+        <MainImage
+          imgUrl={imgUrl}
+          alternativeText={heroImage.alternativeText}
         />
       )}
+
       <h2 className="fs--9 fw--normal ta--center">{name}</h2>
       <ul
         className={`${styles.List} ta--center mb--7`}
@@ -26,32 +99,12 @@ export function DetailPageLayout({ heroImage, name, blog, round }) {
       >
         {round && <Tag>Round {round}</Tag>}
       </ul>
-      {/* <details className={styles.YouTube}>
-        <summary>Sound Test Video</summary>
-        <div className={styles.EmbedWrapper}>
-          <iframe
-            width="100%"
-            height="auto"
-            src="https://www.youtube.com/embed/g-vBr9a36NE"
-            frameBorder="0"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-            allowFullScreen
-          ></iframe>
-        </div>
-      </details> */}
 
       {blog && (
         <div className={`${styles.Post}`}>
           <ReactMarkdown source={blog} />
         </div>
       )}
-      {/* <div className={styles.Gallery}>
-        <img
-          className={styles.Image}
-          src="/keyboards/photos/ava-yellow.jpg"
-          alt="ava"
-        />
-      </div> */}
     </div>
   );
 }
