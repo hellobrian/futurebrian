db51cda9cf47337f1ba399d76aa4714c5901c049
hellobrian <brianhan@hey.com>
2021-04-12 17:22:28 -0500

    setup cloudinary and Gallery component using react-query

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,18 @@
 import { PageLayout } from "@/layouts/PageLayout";
+import { QueryClient, QueryClientProvider } from "react-query";
 import "../styles/globals.css";
 import "react-toggle/style.css";
 
+// Create a client
+const queryClient = new QueryClient();
+
 function MyApp({ Component, pageProps }) {
   return (
-    <PageLayout>
-      <Component {...pageProps} />
-    </PageLayout>
+    <QueryClientProvider client={queryClient}>
+      <PageLayout>
+        <Component {...pageProps} />
+      </PageLayout>
+    </QueryClientProvider>
   );
 }
 
