04ff5ea5a401334d0e1e13ace029fa5a82698828
Brian Han <hellobrian@users.noreply.github.com>
2021-04-18 19:47:17 -0500

    remove strapi main image and use cloudinary only (#12)
    * remove strapi main image and use cloudinary only
    
    * remove queries for hero_image
    
    * create image viewer without prev or next, just click on thumbnails for image view
    
    * Add loading spinner

--- layouts/DetailPageLayout.js
+++ layouts/DetailPageLayout.js
@@ -1,94 +1,157 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import ReactMarkdown from "react-markdown";
 import { useQuery } from "react-query";
 import { CloudinaryContext, Transformation, Image } from "cloudinary-react";
+import Skeleton from "react-loading-skeleton";
 
 import styles from "./DetailPageLayout.module.css";
 import { YouTube } from "@/components/YouTube/YouTube";
-
-const strapiUrl = process.env.NEXT_PUBLIC_PROD_URL;
 
 function Tag({ children }) {
   return <li className={styles.ListItem}>{children}</li>;
 }
 
-function MainImage({ imgUrl, alternativeText }) {
+function Loading() {
   return (
-    <div className={styles.MainImage}>
-      <img data-src={imgUrl} src={imgUrl} alt={alternativeText} />
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        width: 800,
+        height: 800,
+      }}
+    >
+      <svg
+        className={styles.Loading}
+        style={{ width: 48, height: 48 }}
+        viewBox="0 0 300 100"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="white"
+      >
+        <circle cx="50" cy="50" r="50" />
+      </svg>
     </div>
   );
 }
 
+function Thumbnail({ setImage, setSkeleton, img, mainImage }) {
+  const [clicked, setClicked] = useState(false);
+  return (
+    <button
+      type="button"
+      key={img.public_id}
+      onClick={() => {
+        setImage(img.public_id);
+        if (!clicked) {
+          setSkeleton(true);
+          setClicked(true);
+        }
+      }}
+      style={
+        mainImage === img.public_id
+          ? {
+              background: "white",
+              border: "4px solid white",
+            }
+          : {
+              background: "transparent",
+              border: "4px solid transparent",
+            }
+      }
+    >
+      <Image publicId={img.public_id}>
+        <Transformation
+          crop="scale"
+          width="300"
+          dpr="auto"
+          responsive_placeholder="blank"
+        />
+      </Image>
+    </button>
+  );
+}
+
 function Gallery({ name }) {
+  const [skeleton, setSkeleton] = useState(false);
+  const [stateData, setData] = useState(null);
+  const [mainImage, setImage] = useState(null);
   async function getImages() {
     const res = await fetch(
       `https://res.cloudinary.com/brianhan/image/list/${name}.json`
     );
-    const data = await res.json();
-    return data;
+    const images = await res.json();
+    return images;
   }
   const query = useQuery("images", getImages);
   const { data, status, error, refetch } = query;
 
   useEffect(refetch, [refetch, name]);
+  useEffect(() => setData(data), [data]);
+  useEffect(() => {
+    if (!stateData) return;
+
+    setImage(stateData.resources[0].public_id);
+  }, [setImage, stateData]);
+
+  useEffect(() => {
+    if (!skeleton) return;
+
+    setTimeout(() => setSkeleton(false), 2000);
+  }, [skeleton]);
 
   if (!data || status.error) {
     console.log(error);
     return null;
   }
   if (status === "loading" || !data) {
-    return <div>Loading...</div>;
+    return <Skeleton height={800} />;
   }
 
   return (
     <CloudinaryContext cloudName="brianhan">
-      <div className={styles.GalleryWrapper}>
-        <div className={styles.Gallery}>
-          {data.resources.map((img) => (
-            <a
+      <div className={styles.MainImage}>
+        {skeleton ? (
+          <Loading />
+        ) : (
+          <Image publicId={mainImage}>
+            <Transformation
+              crop="fill"
+              height="800"
+              dpr="auto"
+              responsive_placeholder="blank"
+            />
+          </Image>
+        )}
+      </div>
+      <div className={styles.Gallery}>
+        {data.resources.map((img) => {
+          return (
+            <Thumbnail
               key={img.public_id}
-              target="_blank"
-              rel="noreferrer"
-              href={`https://res.cloudinary.com/brianhan/image/upload/${img.public_id}.jpg`}
-            >
-              <Image publicId={img.public_id}>
-                <Transformation
-                  crop="scale"
-                  width="300"
-                  height="200"
-                  dpr="auto"
-                  responsive_placeholder="blank"
-                />
-              </Image>
-            </a>
-          ))}
-        </div>
+              setImage={setImage}
+              setSkeleton={setSkeleton}
+              img={img}
+              mainImage={mainImage}
+            />
+          );
+        })}
       </div>
     </CloudinaryContext>
   );
 }
 
-export function DetailPageLayout({ heroImage, name, blog, round, videos }) {
-  const imgUrl = heroImage ? `${strapiUrl}${heroImage.url}` : null;
-
+export function DetailPageLayout({ name, blog, round, videos }) {
   return (
     <div className={styles.Container}>
-      {heroImage && (
-        <MainImage
-          imgUrl={imgUrl}
-          alternativeText={heroImage.alternativeText}
-        />
-      )}
-      <Gallery name={name} />
       <h2 className="fs--9 fw--normal ta--center">{name}</h2>
-
       <ul
         className={`${styles.List} ta--center mb--7`}
         style={{ width: "100%" }}
       >
-        {round && <Tag>Round {round}</Tag>}
+        {round ? <Tag>Round {round}</Tag> : null}
       </ul>
+      <Gallery name={name} />
 
       {blog && (
         <div className={`${styles.Post}`}>
