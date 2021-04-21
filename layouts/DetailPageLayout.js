fd2efed6cbc2ce02a36ad6c18487aabc6f92150e
hellobrian <brianhan@hey.com>
2021-04-21 17:23:37 -0500

    move Loading component to own folder

--- layouts/DetailPageLayout.js
+++ layouts/DetailPageLayout.js
@@ -5,33 +5,10 @@ import { CloudinaryContext, Transformation, Image } from "cloudinary-react";
 
 import styles from "./DetailPageLayout.module.css";
 import { YouTube } from "@/components/YouTube/YouTube";
+import { Loading } from "@/components/Loading/Loading";
 
 function Tag({ children }) {
   return <li className={styles.ListItem}>{children}</li>;
-}
-
-function Loading() {
-  return (
-    <div
-      style={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        width: 800,
-        height: 800,
-      }}
-    >
-      <svg
-        className={styles.Loading}
-        style={{ width: 48, height: 48 }}
-        viewBox="0 0 300 100"
-        xmlns="http://www.w3.org/2000/svg"
-        fill="white"
-      >
-        <circle cx="50" cy="50" r="50" />
-      </svg>
-    </div>
-  );
 }
 
 function Thumbnail({ setImage, setSkeleton, img, mainImage }) {
