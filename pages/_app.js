dea6cc2b699b807e2a7e6f17bbca2c03d8e99190
hellobrian <brianhan@hey.com>
2021-04-21 17:27:13 -0500

    Remove react-toggle

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,7 @@
 import { PageLayout } from "@/layouts/PageLayout";
 import { QueryClient, QueryClientProvider } from "react-query";
 import "../styles/globals.css";
-import "react-toggle/style.css";
 
-// Create a client
 const queryClient = new QueryClient();
 
 function MyApp({ Component, pageProps }) {
