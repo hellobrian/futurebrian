c225267a5c478f4782a42a11539837699892f958
hellobrian <brianhan@hey.com>
2021-04-23 18:46:21 -0500

    simple socials page

--- components/VideoThumbnail/VideoThumbnail.tsx
+++ components/VideoThumbnail/VideoThumbnail.tsx
@@ -2,12 +2,15 @@ import { IconYoutube } from "@/components/Icons/Icons";
 import styles from "./VideoThumbnail.module.scss";
 
 export function VideoThumbnail(props): JSX.Element {
-  const { text = "video thumbnail", icon = "" } = props;
+  const {
+    headline = "video thumbnail",
+    icon = "",
+    href = "",
+    title,
+    src,
+  } = props;
   return (
-    <a
-      href="https://www.youtube.com/watch?v=BUlNAQkeew4"
-      className={styles.VideoThumbnail}
-    >
+    <a href={href} className={styles.VideoThumbnail}>
       <video
         id="video"
         muted
@@ -16,12 +19,12 @@ export function VideoThumbnail(props): JSX.Element {
         autoPlay
         className={styles.Video}
       >
-        <source
-          src="https://res.cloudinary.com/brianhan/video/upload/v1619200933/L7MX7j_yeeuv3.mp4"
-          type="video/mp4"
-        />
+        <source src={src} type="video/mp4" />
       </video>
-      <span className={styles.Text}>{text}</span>
+      <span className={styles.Text}>
+        <span className={styles.Headline}>{headline}</span>
+        <span className={styles.Title}>{title}</span>
+      </span>
       {icon === "youtube" && (
         <span className={styles.Icon}>
           Watch on YouTube <IconYoutube />
--- pages/socials.tsx
+++ pages/socials.tsx
@@ -1,20 +1,80 @@
+import { request, gql } from "graphql-request";
+import { GetStaticProps } from "next";
+
 import { PageLayout } from "@/components/PageLayout/PageLayout";
 import { VideoThumbnail } from "@/components/VideoThumbnail/VideoThumbnail";
 
 import styles from "@/styles/Socials.module.scss";
 
-export default function Socials(): JSX.Element {
+const ENDPOINT = process.env.PROD_GRAPHQL_ENDPOINT;
+
+export const getStaticProps: GetStaticProps = async () => {
+  const query = gql`
+    query {
+      videos {
+        name
+        id
+        direct_link
+        thumbnail_src
+      }
+    }
+  `;
+
+  const data = await request(ENDPOINT, query);
+  const { videos } = data;
+  return {
+    props: { videos },
+    revalidate: 1,
+  };
+};
+
+export default function Socials({ videos }): JSX.Element {
+  const latestVideo = videos.filter((_, i) => videos.length - 1 === i)[0];
+  console.log(latestVideo);
   return (
     <PageLayout>
       <div className={"page-title"}>
         <h2>Socials</h2>
       </div>
-      <div className={styles.VideoThumbnailList}>
-        <VideoThumbnail icon="youtube" />
-        <VideoThumbnail icon="youtube" />
-        <VideoThumbnail icon="youtube" />
-        <VideoThumbnail icon="youtube" />
-      </div>
+      <section
+        className={styles.Section}
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <div className={styles.TextWrapper}>
+          <p>
+            Here's where you can find my latest videos on{" "}
+            <a href="https://www.youtube.com/channel/UCQGq3OYhoZJrlRaemSCe6Zg">
+              YouTube.
+            </a>
+          </p>
+          <p>
+            I'm also on{" "}
+            <a href="https://www.instagram.com/futurebrian_/">Instagram</a> and
+            working my way into streaming more on{" "}
+            <a href="https://www.twitch.tv/futurebrian">Twitch</a>.
+          </p>
+        </div>
+        <div className={styles.VideoThumbnailList}>
+          <VideoThumbnail
+            icon="youtube"
+            headline="latest video"
+            title={latestVideo.name}
+            href={latestVideo.direct_link}
+            src={latestVideo.thumbnail_src}
+          />
+        </div>
+        <div className={styles.TextWrapper}>
+          <p>
+            If you've been enjoying my content, thank you so much! I appreciate
+            you and it's been a blast connecting with you and everyone through
+            this hobby.
+          </p>
+        </div>
+      </section>
     </PageLayout>
   );
 }
