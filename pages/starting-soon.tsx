dfb55a9ef8d872bc425132b97bf502b609bf9760
hellobrian <brianhan@hey.com>
2021-05-02 23:50:56 -0500

    setup gif interval on starting-soon page

--- pages/starting-soon.tsx
+++ pages/starting-soon.tsx
@@ -1,17 +1,36 @@
+import { useState, useRef, useEffect } from "react";
+
 import { PageLayout } from "@/components/PageLayout/PageLayout";
 import { VariantEnum } from "@/utils/types";
 
 import styles from "@/styles/Home.module.scss";
 
-const gif1 =
-  "https://media0.giphy.com/media/l3fQgFaM1XzWDaf6M/giphy.gif?cid=ecf05e4763odowp0yje45jjdygmef16hmwlkelpmn76n74bo&rid=giphy.gif&ct=g";
-const gif2 =
-  "https://media0.giphy.com/media/3o6ozyUiI5bAKck0BG/giphy.gif?cid=ecf05e47p3v4e96zxxxobpdgcykyit85139h7y7pkxcpttf8&amp;rid=giphy.gif&amp;ct=g";
-
-const dog =
-  "https://media0.giphy.com/media/GiWEowj3nQv9C/giphy.gif?cid=ecf05e4724hqzpfdz43mukf8qirwmntyiu53gsidj0tyymy6&rid=giphy.gif&ct=g";
+const gifs = [
+  "https://media0.giphy.com/media/l3fQgFaM1XzWDaf6M/giphy.gif?cid=ecf05e4763odowp0yje45jjdygmef16hmwlkelpmn76n74bo&rid=giphy.gif&ct=g",
+  "https://media0.giphy.com/media/3o6ozyUiI5bAKck0BG/giphy.gif?cid=ecf05e47p3v4e96zxxxobpdgcykyit85139h7y7pkxcpttf8&amp;rid=giphy.gif&amp;ct=g",
+  "https://media0.giphy.com/media/GiWEowj3nQv9C/giphy.gif?cid=ecf05e4724hqzpfdz43mukf8qirwmntyiu53gsidj0tyymy6&rid=giphy.gif&ct=g",
+];
 
 export default function Commands(): JSX.Element {
+  const [count, setCount] = useState(0);
+  const maxCount = gifs.length - 1;
+  const duration = 1000 * 10;
+  const countRef = useRef(count);
+  countRef.current = count;
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (count >= maxCount) {
+        setCount(0);
+      } else {
+        setCount(count + 1);
+      }
+    }, duration);
+    return () => {
+      clearInterval(interval);
+    };
+  });
+
   return (
     <PageLayout variant={VariantEnum.Twitch}>
       <div className={"page-title"} style={{ marginTop: 32 }}>
@@ -28,7 +47,7 @@ export default function Commands(): JSX.Element {
         <div>
           <div>
             <img
-              src={dog}
+              src={gifs[count]}
               alt="well be right back eric andre GIF by The Eric Andre Show"
             />
           </div>
